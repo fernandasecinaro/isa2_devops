7fe69e19b11dfa34156afad453fb21803d4d7174
Fernanda <fernanda.secinaro@innovant.us>
2022-04-28 09:49:01 -0300

    primer

--- ISA2/ISA2/Program.cs
+++ ISA2/ISA2/Program.cs
@@ -7,6 +7,7 @@ namespace ISA2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World 2!");
         }
     }
 }
